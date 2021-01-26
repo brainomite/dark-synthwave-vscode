a2dd511fa2507c08f45b812b617323eb0aa231b3
Aaron Young <hi@aaronyoung.dev>
2021-01-25 22:50:57 -0500

    feat: Update colors for theme

--- src/js/theme_template.js
+++ src/js/theme_template.js
@@ -12,22 +12,22 @@
     }
 
     var initialThemeStyles = themeStyleTag.innerText;
-    
+
     var updatedThemeStyles = initialThemeStyles;
-    
+
     if (!disableGlow) {
       /* replace neon red */
       updatedThemeStyles = updatedThemeStyles.replace(/color: #fe4450;/g, "color: #fff5f6; text-shadow: 0 0 2px #000, 0 0 10px #fc1f2c[NEON_BRIGHTNESS], 0 0 5px #fc1f2c[NEON_BRIGHTNESS], 0 0 25px #fc1f2c[NEON_BRIGHTNESS];");
-      
+
       /* replace neon pink */
       updatedThemeStyles = updatedThemeStyles.replace(/color: #ff7edb;/g, "color: #f92aad; text-shadow: 0 0 2px #100c0f, 0 0 5px #dc078e33, 0 0 10px #fff3;");
-      
+
       /* replace yellow */
       updatedThemeStyles = updatedThemeStyles.replace(/color: #fede5d;/g, "color: #f4eee4; text-shadow: 0 0 2px #393a33, 0 0 8px #f39f05[NEON_BRIGHTNESS], 0 0 2px #f39f05[NEON_BRIGHTNESS];");
-      
+
       /* replace green */
       updatedThemeStyles = updatedThemeStyles.replace(/color: #72f1b8;/g, "color: #72f1b8; text-shadow: 0 0 2px #100c0f, 0 0 10px #257c55[NEON_BRIGHTNESS], 0 0 35px #212724[NEON_BRIGHTNESS];");
-      
+
       /* replace blue */
       updatedThemeStyles = updatedThemeStyles.replace(/color: #36f9f6;/g, "color: #fdfdfd; text-shadow: 0 0 2px #001716, 0 0 3px #03edf9[NEON_BRIGHTNESS], 0 0 5px #03edf9[NEON_BRIGHTNESS], 0 0 8px #03edf9[NEON_BRIGHTNESS];");
     }
@@ -39,9 +39,9 @@
     newStyleTag.setAttribute("id", "synthwave-84-theme-styles");
     newStyleTag.innerText = updatedThemeStyles.replace(/(\r\n|\n|\r)/gm, '');
     document.body.appendChild(newStyleTag);
-    
+
     console.log('Synthwave \'84: NEON DREAMS initialised!');
-    
+
     // disconnect the observer because we don't need it anymore
     if (obs) {
       obs.disconnect();
